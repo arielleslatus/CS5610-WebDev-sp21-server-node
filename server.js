2d2d35b11fa7cc788419f1d95df6ac999fb03e2a
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-04-23 09:31:19 -0400

    uri const

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ require("./controllers/questions-controller")(app);
 require("./controllers/quiz-attempts-controller")(app);
 require("./controllers/users-controller")(app);
 
-require('dotenv').config();
-app.listen(process.env.PORT || 3001)
 const uri = process.env.MONGODB_URI;
+require('dotenv').config();
+app.listen(uri || 3001)
