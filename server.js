5a9e3d8ab324886e37d6e0f8cec3c589d6960601
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-04-23 18:53:06 -0400

    allow credentials

--- server.js
+++ server.js
@@ -30,11 +30,12 @@ app.use(session({
 }));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://wbdv-sp21-slatus-react.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
                'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
                'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
