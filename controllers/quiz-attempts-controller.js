9382641d59db4d653a64c2ec6911c4dc6cf8d78b
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-04-24 20:27:51 -0400

    controller change

--- controllers/quiz-attempts-controller.js
+++ controllers/quiz-attempts-controller.js
@@ -1,21 +1,31 @@
-const quizAttemptsDao = require('../db/daos/quiz-attempts-dao')
+const quizAttemptsDao = require('../db/daos/quiz-attempts-dao');
 
+// module.exports = (app) => {
+//     const createAttempt = (req, res) => {
+//         const quizId = req.params.qid
+//         const attempt = req.body
+//         quizAttemptsDao.createAttempt(quizId, attempt)
+//             .then(attempt => res.send(attempt))
+//     };
+//
+//     const findAttemptsForQuiz = (req, res) => {
+//         const quizId = req.params.qid
+//         quizAttemptsDao.findAttemptsForQuiz(quizId)
+//             .then(attempts => res.send(attempts))
+//     };
+//
+//
+//
+//     app.post('/api/quizzes/:qid/attempts', createAttempt);
+//     app.get('/api/quizzes/:qid/attempts', findAttemptsForQuiz);
+// };
+
+const quizAttemptDao = require('../daos/quiz-attempts-dao');
 module.exports = (app) => {
-    const createAttempt = (req, res) => {
-        const quizId = req.params.qid
-        const attempt = req.body
-        quizAttemptsDao.createAttempt(quizId, attempt)
-            .then(attempt => res.send(attempt))
-    };
-
-    const findAttemptsForQuiz = (req, res) => {
-        const quizId = req.params.qid
-        quizAttemptsDao.findAttemptsForQuiz(quizId)
-            .then(attempts => res.send(attempts))
-    };
-
-
-
-    app.post('/api/quizzes/:qid/attempts', createAttempt)
-    app.get('/api/quizzes/:qid/attempts', findAttemptsForQuiz)
+    app.post('/api/quizzes/:qid/attempts', (req, res) =>
+        quizAttemptsDao.createAttempt( req.params.qid, req.body)
+            .then(attempt => res.send(attempt)));
+    app.get('/api/quizzes/:qid/attempts', (req, res) =>
+        quizAttemptsDao.findAttemptsForQuiz(req.params.qid)
+            .then(attempts => res.send(attempts)));
 };
