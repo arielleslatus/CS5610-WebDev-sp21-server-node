523eb9ea39568b049d36b3733bef2831c8f9c588
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-04-24 20:33:31 -0400

    ;

--- controllers/quiz-attempts-controller.js
+++ controllers/quiz-attempts-controller.js
@@ -2,14 +2,14 @@ const quizAttemptsDao = require('../db/daos/quiz-attempts-dao');
 
 module.exports = (app) => {
     const createAttempt = (req, res) => {
-        const quizId = req.params.qid
+        const quizId = req.params.qid;
         const attempt = req.body
         quizAttemptsDao.createAttempt(quizId, attempt)
             .then(attempt => res.send(attempt))
     };
 
     const findAttemptsForQuiz = (req, res) => {
-        const quizId = req.params.qid
+        const quizId = req.params.qid;
         quizAttemptsDao.findAttemptsForQuiz(quizId)
             .then(attempts => res.send(attempts))
     };
